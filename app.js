6e9d221c1b0d00ff2a9fad7197ccea3e23f4f305
Adrian Jordan <adejord@gmail.com>
2022-08-18 17:15:06 +0100

    Added code in app.js to ping every 5 mins to avoid sleeping

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/user-routes');
-const HttpError = require('./models/http-error');
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/user-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/uploads/images', express.static(path.join('uploads', 'images')))
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
   next();
 });
 
-app.use('/api/places', placesRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
@@ -43,19 +43,24 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred!' });
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
 mongoose
   .connect(
     // `mongodb+srv://AdeJordan:12477560@cluster0.cs0shny.mongodb.net/mern?retryWrites=true&w=majority`
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cs0shny.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-    )
-    .then(() => {
-      app.listen( process.env.PORT || 5000);
-      // console.log(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cs0shny.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  
+  )
+  .then(() => {
+    app.listen(5000);
+    // console.log(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cs0shny.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  // pings backend every 5 mins to avoid sleeping
+var http = require("http");
+setInterval(function () {
+  http.get("http://place-logger.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
